d393062f8910bdb74b297ae54783235cfd7d9e15
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-12-27 23:41:52 +0100

    Added volumes and claims to db deployment

--- app/frontend/static/js/index.js
+++ app/frontend/static/js/index.js
@@ -4,38 +4,63 @@ window.onload = function() {
   document.getElementById('comment-form')
     .addEventListener('onsubmit', postComment)
   fillComments();
+  getCommentInputElement().focus();
 }
 
 // postComment Posts a new comment to the backend
 var postComment = function(event) {
   event.preventDefault();
   var message = getMessage();
-  console.log(message);
+  postRequest('/api/comment', {body: message})
+    .then(function() {
+      clearMessageForm();
+      fillComments();
+    })
+    .catch(console.log);
 }
 
 // getMessage Gets the entered input comment
 var getMessage = function() {
-  return document.getElementById('comment-input').value
+  return getCommentInputElement().value
+}
+
+// clearMessageForm Clears the comment input form
+var clearMessageForm = function() {
+  getCommentInputElement().value = '';
+}
+
+// getCommentInputElement Gets the comment input form dom element
+var getCommentInputElement = function() {
+  return document.getElementById('comment-input');
 }
 
 // fillComments Fills the published comments in the html document
 var fillComments = function() {
-  var comments = getComments()
-  document.getElementById('comments').innerHTML = mapCommentsToHtml(comments);
+  getComments()
+    .then(function(comments) {
+      populatePageWithComments(comments);
+    })
+    .catch(function(err) {
+      console.log('Failed to get comments:', err);
+    });
 }
 
 // getComments Retrieves comments from the backend
 var getComments = function() {
-  return [
-    'Trying out kube, its cool',
-    'Hayek is da bomb'
-  ];
+  return getRequest('/api/comment')
+          .then(checkReponse)
 }
 
-// mapCommentsToHtml Create
+// populatePageWithComments
+var populatePageWithComments = function(comments) {
+  var commentsHTML = mapCommentsToHtml(comments);
+  document.getElementById('comments').innerHTML = commentsHTML;
+}
+
+// mapCommentsToHtml Creates a string of comment divs from a array of comments
 var mapCommentsToHtml = function(comments) {
   return comments.map(function(comment) {
-    return createCommentPost(comment);
+    return createCommentPost(comment.body);
   }).join('');
 }
 
@@ -47,4 +72,56 @@ var createCommentPost = function(message) {
     '</div>'
   ];
   return commentDiv.join('');
+}
+
+/* ----- HTTP API functions ----- */
+var BACKEND_HOST = '192.168.99.100:30360';
+
+// toURL Creates a http route by prepending protocol and backend host and port
+var toURL = function(route) {
+  return 'http://' + BACKEND_HOST + route;
+}
+
+// getRequest Creates and executes a get request
+var getRequest = function(route) {
+  return fetch(toURL(route))
+          .then(checkReponse)
+          .then(function(res) {
+            return res.json();
+          });
+}
+
+// postRequest Creates and executes a post request
+var postRequest = function(route, body) {
+  return fetch(toURL(route), postRequestObject(body))
+          .then(checkReponse);
+}
+
+// checkReponse Checks if a response has a 200 status
+var checkReponse = function(response) {
+  if (response.status != 500) {
+    return response;
+  } else {
+    var error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
+// postRequestObject Creates a object to to do a post request
+var postRequestObject = function(body) {
+  return makeRequestObject('POST', body);
+}
+
+// makeRequestObject Creates a request object to be used by the FETCH api
+var makeRequestObject = function(method, body) {
+  return {
+    method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    mode: 'no-cors',
+    body: JSON.stringify(body)
+  };
 }
